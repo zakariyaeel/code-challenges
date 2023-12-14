7e77ad4a2fc78b4dcbe094e0ae48f0a4974fcb75
zakariyae <98268057+zakariyaeel@users.noreply.github.com>
2023-12-14 23:45:03 +0100

    Create split-regex.js

---
+++ freeCodeCamp/basic-algorithm/split-regex.js
@@ -0,0 +1,8 @@
+function splitify(str) {
+  // Only change code below this line
+
+return str.split(/\s|[-,.]/)
+  // Only change code above this line
+}
+
+console.log(splitify("Hello World,I-am code"));
